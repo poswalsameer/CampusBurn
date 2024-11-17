98072ba2f5be746a0d859b2de740504f6eb49b02
poswalsameer <poswalsameer2003@gmail.com>
2024-11-18 00:09:40 +0530

    adding signup using clerk

---
+++ client/campusburn/src/app/sign-up/[[...sign-up]]/page.tsx
@@ -0,0 +1,11 @@
+import { SignUp } from '@clerk/nextjs'
+
+export default function Page() {
+  return (
+
+    <div className='min-h-screen w-full flex justify-center items-center' >
+        <SignUp />
+    </div>
+
+  ) 
+}
