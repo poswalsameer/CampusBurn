18e269df7332652b3a525f1b29c1c3bc95910ded
nameerakhter <akhtarnameer@gmail.com>
2024-12-07 22:36:13 +0530

    update Loading spinner

--- client/campusburn/src/app/Loading.tsx
+++ client/campusburn/src/app/Loading.tsx
@@ -4,11 +4,8 @@ const LoadingSpinner = () => {
   return (
     <div className="fixed inset-0 bg-black/50 flex items-center justify-center z-50">
       <div className="relative">
-        {/* Outer spinning ring */}
+        {/* Simple spinning ring */}
         <div className="w-16 h-16 border-4 border-blue-400 border-t-transparent rounded-full animate-spin"></div>
-        
-        {/* Inner spinning ring */}
-        <div className="absolute top-1 left-1 w-14 h-14 border-4 border-orange-400 border-t-transparent rounded-full animate-spin"></div>
         
         {/* Loading text */}
         <div className="mt-4 text-center text-blue-200 font-semibold">
