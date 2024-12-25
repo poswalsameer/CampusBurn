b32f485adf5d662ab38329e6703da547ded7ad6c
poswalsameer <poswalsameer2003@gmail.com>
2024-12-25 23:09:53 +0530

    added comment for code readability

--- server/controller/posts/getAllPosts.go
+++ server/controller/posts/getAllPosts.go
@@ -7,6 +7,15 @@ import (
 	"github.com/gofiber/fiber/v2"
 )
 
+/*
+FLOW TO GET ALL THE POSTS:
+--------------------------
+1. Create a variable array to store the posts.
+2. Get all the posts from the database.
+3. If any error while finding the posts, then return error response.
+4. Return the success response with all posts as the data.
+*/
+
 func GetAllPosts(c *fiber.Ctx) error {
 
 	var allPosts []model.Post
